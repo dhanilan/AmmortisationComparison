7955b3ad6186187b72e2c6b35a78c43d066df33c
INDSANKA <INDSANKA@INBLRN0446>
2016-12-07 14:03:22 +0530

    Add project files.

---
+++ Ammortisation/EMI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ammortisation
+{
+    public class EMI
+    {
+        public double Principal { get; set; }
+        public double RateOfInterest { get; set; }
+
+        public double MonthlyFixedRepaymentGreaterThanEMI { get; set; }
+
+        public int LoanTenureMonths { get; set; }
+        public InterestCalculationBasis InterestCalculationBasis { get; set; }
+
+        private double RateOfInterestPerMonth
+        {
+            get
+            {
+
+                return RateOfInterest / 12 / 100;
+            }
+        }
+
+        public EMI()
+        {
+
+        }
+
+        public double GetEMIAmount()
+        {
+            var oneplusrpowerN = Math.Pow(1 + RateOfInterestPerMonth, LoanTenureMonths);
+            return Principal * RateOfInterestPerMonth * (oneplusrpowerN / (oneplusrpowerN - 1));
+        }
+
+        public List<MontlyBreakUp> GetBreakUp()
+        {
+            var result = new List<MontlyBreakUp>();
+
+            var principalLeft = Principal;
+            var emi = GetEMIAmount();
+
+            for (int i = 0;( i < LoanTenureMonths )&& (principalLeft > 0); i++)
+            {
+                var interestforMonthI = GetInterestForAmount(principalLeft);
+                var principalforMonthI = MonthlyFixedRepaymentGreaterThanEMI>0 ?(MonthlyFixedRepaymentGreaterThanEMI - interestforMonthI)   :(emi - interestforMonthI);
+                principalLeft = principalLeft< principalforMonthI?0: principalLeft - principalforMonthI;
+                result.Add(new MontlyBreakUp() { InterestComponent = interestforMonthI, Month = i, PrincipalComponent = principalforMonthI, PrincipalLeft= principalLeft });
+            }
+            return result;
+        }
+
+        public double GetInterestForAmount(double principal)
+        {
+            return principal * this.RateOfInterestPerMonth;
+        }
+    }
+
+    public enum InterestCalculationBasis
+    {
+
+        Monthly,
+        Yearly
+    }
+
+    public class MontlyBreakUp
+    {
+        public int Month { get; set; }
+        public double InterestComponent { get; set; }
+        public double PrincipalComponent { get; set; }
+        public double PrincipalLeft { get; set; }
+    }
+}
---
+++ Ammortisation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ammortisation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PrincipalAmount = 270000;//Loan Amount
+            const double AnnualRateOfInterest = 10.5;//Loan Amount
+            const int LoanTenure = 180;
+            const int ExtendedLoanTenure = 240;
+
+            var emicalcForNormalTenure =  new EMI()
+            {
+                LoanTenureMonths = LoanTenure,
+                Principal = PrincipalAmount,
+                RateOfInterest = AnnualRateOfInterest
+            };
+
+            var fixedEMI = emicalcForNormalTenure.GetEMIAmount();
+           
+            var emiwithExtendedTenure = new EMI()
+            {
+                LoanTenureMonths = ExtendedLoanTenure,
+                Principal = PrincipalAmount,
+                RateOfInterest = AnnualRateOfInterest,
+                MonthlyFixedRepaymentGreaterThanEMI = fixedEMI
+            };
+
+            Console.WriteLine("==============RESULTS==============");
+
+            Console.WriteLine("==============Months Taken to complete Loan: "+ emicalcForNormalTenure.GetBreakUp().Count);
+            Console.WriteLine("==============Months Taken to complete Extended Loan: " + emiwithExtendedTenure.GetBreakUp().Count);
+
+            Console.WriteLine("==============EMI for normal loan: " + fixedEMI);
+            Console.WriteLine("==============EMI for extended loan: " + fixedEMI);
+            Console.ReadLine();
+        }
+    }
+}
